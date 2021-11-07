2256b9cc82e1b7623a19b617de36814e7f698e1e
Mantas Zalnierius <C00241626@itcarlow.ie>
2021-11-07 19:36:51 +0000

    Added tests for Left And Right Movement (#2)

--- 2D Movement/Packages/ie.itcarlow.2dmovement/2DMovementTests/Tests2DMovementTests.cs
+++ 2D Movement/Packages/ie.itcarlow.2dmovement/2DMovementTests/Tests2DMovementTests.cs
@@ -1,28 +1,41 @@
 ﻿using System.Collections;
-using System.Collections.Generic;
 using NUnit.Framework;
 using UnityEngine;
 using UnityEngine.TestTools;
+using UnityEngine.SceneManagement;
 
 namespace Tests
 {
-    public class Tests2DMovementTests
+    public class Movement
     {
-        // A Test behaves as an ordinary method
-        [Test]
-        public void Tests2DMovementTestsSimplePasses()
+        private GameObject player;
+
+        [SetUp]
+        public void Setup()
         {
-            // Use the Assert class to test conditions
+            SceneManager.LoadScene("TestScene", LoadSceneMode.Single);
         }
 
-        // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
-        // `yield return null;` to skip a frame.
-        [UnityTest]
-        public IEnumerator Tests2DMovementTestsWithEnumeratorPasses()
+        [TearDown]
+        public void Teardown()
         {
-            // Use the Assert class to test conditions.
-            // Use yield to skip a frame.
-            yield return null;
+            Object.Destroy(player);
+            SceneManager.UnloadSceneAsync("TestScene");
+        }
+
+        [UnityTest]
+        public IEnumerator HorizontalMovement()
+        {
+            player = GameObject.Find("Player");
+            Vector3 position = player.GetComponent<Rigidbody2D>().transform.position;
+            player.GetComponent<Runtime2DMovement>().moveRight();
+            yield return new WaitForSeconds(1.0f);
+            Assert.Greater(player.GetComponent<Rigidbody2D>().position.x, position.x);
+
+            position = player.GetComponent<Rigidbody2D>().position;
+            player.GetComponent<Runtime2DMovement>().moveLeft();
+            yield return new WaitForSeconds(1.0f);
+            Assert.Less(player.GetComponent<Rigidbody2D>().position.x, position.x);
         }
     }
 }
