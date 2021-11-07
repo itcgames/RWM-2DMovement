f2547e2cdc7ef869adb7182e530a27aa9c950a25
Mantas Zalnierius <C00241626@itcarlow.ie>
2021-11-07 20:20:30 +0000

    Have jumping implemented will work on tests now

--- 2D Movement/Packages/ie.itcarlow.2dmovement/Runtime/Runtime2DMovement.cs
+++ 2D Movement/Packages/ie.itcarlow.2dmovement/Runtime/Runtime2DMovement.cs
@@ -6,13 +6,18 @@ public class Runtime2DMovement : MonoBehaviour
 {
     private bool _moveRight; // right direction.
     private bool _moveLeft;
+    private bool _isJumping;
     private Rigidbody2D rb;
     private bool _isGrounded; // is the player on the ground.
+    private float jumpTimeCounter;
 
-    public int _speed;
+    public int _Walkingspeed;
     public string _walkableSurfaceTagName;
     public KeyCode leftKey;
     public KeyCode rightKey;
+    public KeyCode jumpKey;
+    public float impluseJumpVel;
+    public float TimeToReachMaxHeight;
 
     void Start()
     {
@@ -27,7 +32,10 @@ public class Runtime2DMovement : MonoBehaviour
         _moveRight = false;
         _moveLeft = false;
         _isGrounded = false;
-        _speed = 2;
+        _Walkingspeed = 2;
+
+        impluseJumpVel = 4f;
+        TimeToReachMaxHeight = 0.5f;
     }
 
     void Update()
@@ -40,6 +48,7 @@ public class Runtime2DMovement : MonoBehaviour
     {
         getRightInput();
         getLightInput();
+        getUpInput();
         move();
     }
 
@@ -86,11 +95,28 @@ public class Runtime2DMovement : MonoBehaviour
         }
     }
 
+    void getUpInput()
+    {
+        if (Input.GetKeyDown(jumpKey) && _isGrounded) // UP
+        {
+            intialJump();
+        }
+        if (Input.GetKey(jumpKey) && _isJumping)
+        {
+            continuousJump();
+        }
+        if (Input.GetKeyUp(jumpKey))
+        {
+            _isJumping = false;
+        }
+    }
+
     private void OnCollisionEnter2D(Collision2D walkableSurface)
     {
         if (walkableSurface.gameObject.tag == _walkableSurfaceTagName && !_isGrounded)
         {
             _isGrounded = true;
+            _isJumping = false;
         }
     }
 
@@ -106,14 +132,14 @@ public class Runtime2DMovement : MonoBehaviour
     public void moveRight()
     {
         Vector2 temp = rb.velocity;
-        temp.x = _speed;
+        temp.x = _Walkingspeed;
         rb.velocity = temp;
     }
 
     public void moveLeft()
     {
         Vector2 temp = rb.velocity;
-        temp.x = -_speed;
+        temp.x = -_Walkingspeed;
         rb.velocity = temp;
     }
 
@@ -122,5 +148,31 @@ public class Runtime2DMovement : MonoBehaviour
         Vector2 temp = rb.velocity;
         temp.x = 0;
         rb.velocity = temp;
+    }
+
+    void intialJump()
+    {
+        Vector3 temp = rb.velocity;
+        temp = Vector2.up * impluseJumpVel; // Impluse megaman into the air by a set amount.
+        temp.x = rb.velocity.x;
+        rb.velocity = temp;
+        jumpTimeCounter = TimeToReachMaxHeight; // reset jumptimecounter.
+        _isJumping = true;
+    }
+
+    void continuousJump()
+    {
+        if (jumpTimeCounter > 0)
+        {
+            Vector3 temp = rb.velocity;
+            temp = Vector2.up * impluseJumpVel;
+            temp.x = rb.velocity.x;
+            rb.velocity = temp;
+            jumpTimeCounter -= Time.deltaTime;
+        }
+        else // Else he is falling.
+        {
+            _isJumping = false;
+        }
     }
 }
