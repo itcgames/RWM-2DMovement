4acbdce8eee76007c9488c5dd5b7cb23711925fa
Mantas Zalnierius <C00241626@itcarlow.ie>
2021-11-07 17:50:55 +0000

    Finished walking right movement

--- 2D Movement/Packages/ie.itcarlow.2dmovement/Runtime/Runtime2DMovement.cs
+++ 2D Movement/Packages/ie.itcarlow.2dmovement/Runtime/Runtime2DMovement.cs
@@ -4,15 +4,75 @@ using UnityEngine;
 
 public class Runtime2DMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private bool _moveRight; // right direction.
+    private Rigidbody2D rb;
+    public bool _isGrounded; // is the player on the ground.
+    public int _speed;
+    public string _walkableSurfaceTagName;
     void Start()
     {
-        
+        rb = this.GetComponent<Rigidbody2D>();
+        if (!rb)
+        {
+            gameObject.AddComponent<Rigidbody2D>();
+            rb = GetComponent<Rigidbody2D>();
+            rb.angularDrag = 0;
+            rb.constraints = RigidbodyConstraints2D.FreezeRotation;
+        }
+        _moveRight = false;
+        _isGrounded = false;
+        _speed = 2;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        getInput();
+        if(_moveRight)
+        {
+            Vector2 temp = rb.velocity;
+            temp.x = _speed;
+            rb.velocity = temp;
+        }
+        else
+        {
+            Vector2 temp = rb.velocity;
+            temp.x = 0;
+            rb.velocity = temp;
+        }
+    }
+
+    // Gets input for the user.
+    void getInput()
+    {
+        getRightInput();
+    }
+
+    void getRightInput()
+    {
+        if (Input.GetKeyDown(KeyCode.D)) // RIGHT
+        {
+            _moveRight = true;
+        }
+        if (Input.GetKeyUp(KeyCode.D))
+        {
+            _moveRight = false;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D walkableSurface)
+    {
+        if (walkableSurface.gameObject.tag == _walkableSurfaceTagName && !_isGrounded)
+        {
+            _isGrounded = true;
+        }
+    }
+
+
+    private void OnCollisionExit2D(Collision2D walkableSurface)
+    {
+        if (walkableSurface.gameObject.tag == _walkableSurfaceTagName && _isGrounded)
+        {
+            _isGrounded = false;
+        }
     }
 }
