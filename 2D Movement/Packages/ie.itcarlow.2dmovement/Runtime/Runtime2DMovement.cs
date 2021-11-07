ec4ef754d1501335b62d3a173d330a0ba1c50dac
Mantas Zalnierius <C00241626@itcarlow.ie>
2021-11-07 18:42:29 +0000

    Left and right movement (#1)
    * Finished walking right movement
    
    * Added in left movement and custom keys

--- 2D Movement/Packages/ie.itcarlow.2dmovement/Runtime/Runtime2DMovement.cs
+++ 2D Movement/Packages/ie.itcarlow.2dmovement/Runtime/Runtime2DMovement.cs
@@ -4,15 +4,108 @@ using UnityEngine;
 
 public class Runtime2DMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private bool _moveRight; // right direction.
+    private bool _moveLeft;
+    private Rigidbody2D rb;
+    private bool _isGrounded; // is the player on the ground.
+
+    public int _speed;
+    public string _walkableSurfaceTagName;
+    public KeyCode leftKey;
+    public KeyCode rightKey;
+
     void Start()
     {
-        
+        rb = this.GetComponent<Rigidbody2D>();
+        if (!rb)
+        {
+            gameObject.AddComponent<Rigidbody2D>();
+            rb = GetComponent<Rigidbody2D>();
+            rb.angularDrag = 0;
+            rb.constraints = RigidbodyConstraints2D.FreezeRotation;
+        }
+        _moveRight = false;
+        _moveLeft = false;
+        _isGrounded = false;
+        _speed = 2;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        getInput();
+    }
+
+    // Gets input for the user.
+    void getInput()
+    {
+        getRightInput();
+        getLightInput();
+        move();
+    }
+
+    void move()
+    {
+        if (_moveRight || _moveLeft)
+        {
+            if (_moveRight)
+            {
+                Vector2 temp = rb.velocity;
+                temp.x = _speed;
+                rb.velocity = temp;
+            }
+            else if (_moveLeft)
+            {
+                Vector2 temp = rb.velocity;
+                temp.x = -_speed;
+                rb.velocity = temp;
+            }
+        }
+        else
+        {
+            Vector2 temp = rb.velocity;
+            temp.x = 0;
+            rb.velocity = temp;
+        }
+    }
+
+    void getRightInput()
+    {
+        if (Input.GetKeyDown(rightKey)) // RIGHT
+        {
+            _moveRight = true;
+        }
+        if (Input.GetKeyUp(rightKey))
+        {
+            _moveRight = false;
+        }
+    }
+
+    void getLightInput()
+    {
+        if (Input.GetKeyDown(leftKey)) // RIGHT
+        {
+            _moveLeft = true;
+        }
+        if (Input.GetKeyUp(leftKey))
+        {
+            _moveLeft = false;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D walkableSurface)
+    {
+        if (walkableSurface.gameObject.tag == _walkableSurfaceTagName && !_isGrounded)
+        {
+            _isGrounded = true;
+        }
+    }
+
+
+    private void OnCollisionExit2D(Collision2D walkableSurface)
+    {
+        if (walkableSurface.gameObject.tag == _walkableSurfaceTagName && _isGrounded)
+        {
+            _isGrounded = false;
+        }
     }
 }
