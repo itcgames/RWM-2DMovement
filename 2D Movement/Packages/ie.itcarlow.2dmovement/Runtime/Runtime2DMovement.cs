154285e5662ea9df3af0b638611b51688b0057ef
Mantas Zalnierius <C00241626@itcarlow.ie>
2021-11-08 00:15:35 +0000

    Added ADSR enevelope to ease out/in movement with tests passing

--- 2D Movement/Packages/ie.itcarlow.2dmovement/Runtime/Runtime2DMovement.cs
+++ 2D Movement/Packages/ie.itcarlow.2dmovement/Runtime/Runtime2DMovement.cs
@@ -4,52 +4,77 @@ using UnityEngine;
 
 public class Runtime2DMovement : MonoBehaviour
 {
-    private bool _moveRight; // right direction.
+    // BOOLS USED FOR MOVEMENT, WILL BE CHANGED FOR FSM
+    private bool _moveRight;
     private bool _moveLeft;
     private bool _isJumping;
-    private Rigidbody2D rb;
-    private bool _isGrounded; // is the player on the ground.
-    private float jumpTimeCounter;
+    private bool _isGrounded;
+    // BOOLS USED FOR MOVEMENT, WILL BE CHANGED FOR FSM
 
-    public int _Walkingspeed;
+    private Rigidbody2D rb;
+    private float jumpTimeCounter;
+    private Vector2 _velocity;
+    private float _timeLeft;
+    private float _declaration;
+    private float _elaspedTimeSinceButtonPress;
+
+    // VARIALBES THE USER CAN EDIT TO CREATE DIFFERENT JUMP ARCS/MOVEMENT
     public string _walkableSurfaceTagName;
     public KeyCode leftKey;
     public KeyCode rightKey;
     public KeyCode jumpKey;
     public float impluseJumpVel;
     public float TimeToReachMaxHeight;
+    public float _movementTime = 0.100f;
+    public float _MAX_WALKING_SPEED;
+    public float acclearation;
+    public float _LOWEST_WALKING_SPEED;
+    // VARIALBES THE USER CAN EDIT TO CREATE DIFFERENT JUMP ARCS/MOVEMENT
 
     void Start()
     {
         rb = this.GetComponent<Rigidbody2D>();
+
+        // CHECK IF THE GAMEOBJECT HAS A RIGIDBODY IF NOT THEN CREATE ONE
         if (!rb)
         {
             gameObject.AddComponent<Rigidbody2D>();
             rb = GetComponent<Rigidbody2D>();
             rb.angularDrag = 0;
             rb.constraints = RigidbodyConstraints2D.FreezeRotation;
+            rb.gravityScale = 3;
         }
+        
+        // CHECK IF THE GAME OBJECT HAS A BOX COLLDER ATTACHED, IF NOT THEN CREATE ONE.
+        if(!this.GetComponent<BoxCollider2D>())
+        {
+            gameObject.AddComponent<BoxCollider2D>();
+        }
+
         _moveRight = false;
         _moveLeft = false;
         _isGrounded = false;
-        _Walkingspeed = 2;
-
+        _declaration = 0.0f;
         impluseJumpVel = 4f;
         TimeToReachMaxHeight = 0.5f;
+        acclearation = 17.0f;
+        _velocity = new Vector2(0.0f, 0.0f);
+        _MAX_WALKING_SPEED = 5.0f;
+        _LOWEST_WALKING_SPEED = 0.3f;
     }
 
     void Update()
     {
         getInput();
+        move();
     }
 
     // Gets input for the user.
     void getInput()
     {
+        getUpInput();
         getRightInput();
         getLightInput();
-        getUpInput();
-        move();
     }
 
     void move()
@@ -64,6 +89,7 @@ public class Runtime2DMovement : MonoBehaviour
             {
                 moveLeft();
             }
+            _elaspedTimeSinceButtonPress += Time.deltaTime;
         }
         else
         {
@@ -75,7 +101,21 @@ public class Runtime2DMovement : MonoBehaviour
     {
         if (Input.GetKeyDown(rightKey)) // RIGHT
         {
+            if (_moveLeft)
+            {
+                _timeLeft = _movementTime * 2.0f;
+            }
+            else
+            {
+                _timeLeft = _movementTime;
+                if (_velocity.x > 0.0f)
+                {
+                    _timeLeft = _MAX_WALKING_SPEED - _velocity.x / acclearation; // t = v - u / a.
+                }
+            }
             _moveRight = true;
+            _moveLeft = false;
+            _elaspedTimeSinceButtonPress = 0.0f;
         }
         if (Input.GetKeyUp(rightKey))
         {
@@ -87,7 +127,21 @@ public class Runtime2DMovement : MonoBehaviour
     {
         if (Input.GetKeyDown(leftKey)) // RIGHT
         {
+            if (_moveRight)
+            {
+                _timeLeft = _movementTime * 2.0f;
+            }
+            else
+            {
+                _timeLeft = _movementTime;
+                if (_velocity.x < 0.0f)
+                {
+                    _timeLeft = _MAX_WALKING_SPEED - _velocity.x / acclearation; // t = v - u / a.
+                }
+            }
             _moveLeft = true;
+            _moveRight = false;
+            _elaspedTimeSinceButtonPress = 0.0f;
         }
         if (Input.GetKeyUp(leftKey))
         {
@@ -118,6 +172,11 @@ public class Runtime2DMovement : MonoBehaviour
             _isGrounded = true;
             _isJumping = false;
         }
+        else
+        {
+            _moveLeft = false;
+            _moveRight = false;
+        }
     }
 
 
@@ -131,23 +190,62 @@ public class Runtime2DMovement : MonoBehaviour
 
     public void moveRight()
     {
-        Vector2 temp = rb.velocity;
-        temp.x = _Walkingspeed;
-        rb.velocity = temp;
+        if (_elaspedTimeSinceButtonPress < _timeLeft)
+        {
+            _velocity = rb.velocity;
+            _velocity.x = getVel(_elaspedTimeSinceButtonPress).x;
+            rb.velocity = _velocity;
+            rb.velocity = new Vector2(Mathf.Clamp(_velocity.x, -_MAX_WALKING_SPEED, _MAX_WALKING_SPEED), _velocity.y); // Clamp speed.
+        }
+        else
+        {
+            Vector3 temp = rb.velocity;
+            temp.x = _MAX_WALKING_SPEED;
+            rb.velocity = temp;
+        }
     }
 
     public void moveLeft()
     {
-        Vector2 temp = rb.velocity;
-        temp.x = -_Walkingspeed;
-        rb.velocity = temp;
+        if (_elaspedTimeSinceButtonPress < _timeLeft) // If the elaspedtime on when the button is pressed is less than 
+        {
+            _velocity = rb.velocity;
+            _velocity.x = -getVel(_elaspedTimeSinceButtonPress).x;
+            rb.velocity = _velocity;
+            rb.velocity = new Vector2(Mathf.Clamp(_velocity.x, -_MAX_WALKING_SPEED, _MAX_WALKING_SPEED), _velocity.y); // Clamp speed.
+        }
+        else
+        {
+            Vector3 temp = rb.velocity;
+            temp.x = -_MAX_WALKING_SPEED;
+            rb.velocity = temp;
+        }
     }
 
     public void stopLeftAndRightMovement()
     {
-        Vector2 temp = rb.velocity;
-        temp.x = 0;
-        rb.velocity = temp;
+        if (_velocity.x != 0.0f)
+        {
+            _declaration = acclearation / _movementTime * Time.deltaTime; // Declaration = v/t.
+            if (_velocity.x < 0.0f)
+            {
+                _velocity.x += _declaration; // reduce vel by declaration.
+                if (_velocity.x >= -_LOWEST_WALKING_SPEED)
+                {
+                    _velocity.x = 0.0f;
+                }
+            }
+            else if (_velocity.x > 0.0f)
+            {
+                _velocity.x -= _declaration; // reduce vel by declaration.
+                if (_velocity.x <= _LOWEST_WALKING_SPEED)
+                {
+                    _velocity.x = 0.0f;
+                }
+            }
+            _velocity.y = rb.velocity.y;
+            rb.velocity = _velocity;
+        }
     }
 
     public void intialJump()
@@ -165,7 +263,7 @@ public class Runtime2DMovement : MonoBehaviour
         if (jumpTimeCounter > 0)
         {
             Vector3 temp = rb.velocity;
-            temp = Vector2.up * impluseJumpVel;
+            temp = Vector2.up * impluseJumpVel * 1.3f;
             temp.x = rb.velocity.x;
             rb.velocity = temp;
             jumpTimeCounter -= Time.deltaTime;
@@ -179,5 +277,10 @@ public class Runtime2DMovement : MonoBehaviour
     public bool getIsGrounded()
     {
         return _isGrounded;
+    }
+
+    Vector2 getVel(float time)
+    {
+        return new Vector3(acclearation * time, _velocity.y, 0.0f); // v = u + at.
     }
 }
