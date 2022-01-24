2fb940b6a4667368db6931895de599a420c0da49
Mantas Zalnierius <C00241626@itcarlow.ie>
2022-01-24 03:51:45 +0000

    Fixed compiler errors

--- 2D Movement/Packages/ie.itcarlow.2dmovement/Runtime/WalkingRightState.cs
+++ 2D Movement/Packages/ie.itcarlow.2dmovement/Runtime/WalkingRightState.cs
@@ -20,17 +20,17 @@ public class WalkingRightState : State
     {
         base.UpdateLogic();
         moveRight();
-        _sm.movement.setTimeSinceLastButtonPress(_sm.movement.getTimeSinceLastButtonPress() + Time.deltaTime);
-        if (Input.GetKeyUp(_sm.movement.rightKey))
+        _sm.movementController.setTimeSinceLastButtonPress(_sm.movementController.getTimeSinceLastButtonPress() + Time.deltaTime);
+        if (Input.GetKeyUp(_sm.movementController.rightKey))
         {
-            _sm.movement.setVelocity(_sm.movement.getRigidBody().velocity);
+            _sm.movementController.setVelocity(_sm.movementController.getRigidBody().velocity);
             stateMachine.ChangeState(_sm.idleState);
         }
-        else if (Input.GetKeyDown(_sm.movement.leftKey))
+        else if (Input.GetKeyDown(_sm.movementController.leftKey))
         {
             stateMachine.ChangeState(_sm.movementLeft);
         }
-        else if (Input.GetKeyDown(_sm.movement.jumpKey) && _sm.movement.getIsGrounded())
+        else if (Input.GetKeyDown(_sm.movementController.jumpKey) && _sm.movementController.getIsGrounded())
         {
             stateMachine.ChangeState(_sm.jumping);
         }
@@ -38,46 +38,46 @@ public class WalkingRightState : State
 
     public void handleRightInput()
     {
-        if (_sm.movement.getIsMovingLeft())
+        if (_sm.movementController.getIsMovingLeft())
         {
-            _sm.movement.setTimeLeft(_sm.movement._movementTime * 2.0f);
+            _sm.movementController.setTimeLeft(_sm.movementController._movementTime * 2.0f);
         }
         else
         {
-            _sm.movement.setTimeLeft(_sm.movement._movementTime);
-            if (_sm.movement.getVelocity().x > 0.0f)
+            _sm.movementController.setTimeLeft(_sm.movementController._movementTime);
+            if (_sm.movementController.getVelocity().x > 0.0f)
             {
-                _sm.movement.setTimeLeft(_sm.movement._MAX_WALKING_SPEED - _sm.movement.getVelocity().x / _sm.movement.acclearation); // t = v - u / a.
+                _sm.movementController.setTimeLeft(_sm.movementController._MAX_WALKING_SPEED - _sm.movementController.getVelocity().x / _sm.movementController.acclearation); // t = v - u / a.
             }
         }
-        _sm.movement.setWalkRight(true);
-        _sm.movement.setWalkLeft(false);
-        _sm.movement.setTimeSinceLastButtonPress(0.0f);
+        _sm.movementController.setWalkRight(true);
+        _sm.movementController.setWalkLeft(false);
+        _sm.movementController.setTimeSinceLastButtonPress(0.0f);
     }
 
     public void moveRight()
     {
-        Vector2 temp = _sm.movement.getVelocity();
-        if (_sm.movement.getTimeSinceLastButtonPress() < _sm.movement.getTimeLeft())
+        Vector2 temp = _sm.movementController.getVelocity();
+        if (_sm.movementController.getTimeSinceLastButtonPress() < _sm.movementController.getTimeLeft())
         {
-            temp = _sm.movement.getVelocity();
-            temp = _sm.movement.getRigidBody().velocity;
-            temp.x = getVel(_sm.movement.getTimeSinceLastButtonPress(), temp.y).x;
-            _sm.movement.setRigidBodyVelocity(temp);
-            _sm.movement.setVelocity(temp);
-            _sm.movement.setRigidBodyVelocity(new Vector2(Mathf.Clamp(temp.x, -_sm.movement._MAX_WALKING_SPEED, _sm.movement._MAX_WALKING_SPEED), temp.y)); // Clamp speed.
+            temp = _sm.movementController.getVelocity();
+            temp = _sm.movementController.getRigidBody().velocity;
+            temp.x = getVel(_sm.movementController.getTimeSinceLastButtonPress(), temp.y).x;
+            _sm.movementController.setRigidBodyVelocity(temp);
+            _sm.movementController.setVelocity(temp);
+            _sm.movementController.setRigidBodyVelocity(new Vector2(Mathf.Clamp(temp.x, -_sm.movementController._MAX_WALKING_SPEED, _sm.movementController._MAX_WALKING_SPEED), temp.y)); // Clamp speed.
         }
         else
         {
-            temp = _sm.movement.getRigidBody().velocity;
-            temp.x = _sm.movement._MAX_WALKING_SPEED;
-            _sm.movement.setRigidBodyVelocity(temp);
-            _sm.movement.setVelocity(temp);
+            temp = _sm.movementController.getRigidBody().velocity;
+            temp.x = _sm.movementController._MAX_WALKING_SPEED;
+            _sm.movementController.setRigidBodyVelocity(temp);
+            _sm.movementController.setVelocity(temp);
         }
     }
 
     Vector2 getVel(float time, float t_yVel)
     {
-        return new Vector3(_sm.movement.acclearation * time, t_yVel, 0.0f); // v = u + at.
+        return new Vector3(_sm.movementController.acclearation * time, t_yVel, 0.0f); // v = u + at.
     }
 }
